36de4f63b21b340fd2c9e9de6f5a02a45387b6ee
Matthew Labrecque <mlabrecque2002@gmail.com>
2026-04-29 16:54:49 -0400

    Add sandboxing prompt

--- pim.py
+++ pim.py
@@ -6,12 +6,11 @@ import sys
 from pathlib import Path
 
 
-# TODO: For this to actually work as intended, enable subprocess execution so killing the script kills pi
 # TODO: add in rm interrupt when removing files
 def sandbox_pi():
     pi_binary = shutil.which("pi")
     if not pi_binary:
-        print("Error. Pi binary found.")
+        print("Error. Pi binary not found.")
         exit(1)
 
     ALLOWED_DIRS = [
@@ -23,6 +22,10 @@ def sandbox_pi():
         Path.home() / ".ssh",
         Path.home() / ".aws",
     ]
+
+    SANDBOX_PROMPT = """Additionally, your computer is sandboxed and you only have access to the current working directory (.), /tmp, and ~/.pi.
+    If the user asks you to make changes to any directory outside of the three specified above, immediately inform the user that 
+    you don't have access to those files and ask what the user wants to do. Also, if you want to remove a file from the project, ask the user for confirmation before removing."""
 
     # TODO: COnfirm this blocks sudo level calls
     cmd = [
@@ -46,7 +49,7 @@ def sandbox_pi():
         if path.exists():
             cmd += ["--tmpfs", str(path.resolve())]
 
-    cmd += ["--", pi_binary] + sys.argv[1:]
+    cmd += ["--", pi_binary, "--append-system-prompt", SANDBOX_PROMPT] + sys.argv[1:]
     os.execvp(cmd[0], cmd)
 
 
